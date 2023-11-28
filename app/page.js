00da66c488cba3352a6f25d9e56f06351eabac23
Pedro Portella <pportella23@gmail.com>
2023-11-28 15:03:55 -0300

    feat: Add author's name and pic @ feed & post page

--- app/page.js
+++ app/page.js
@@ -86,13 +86,28 @@ function PostTitle({ post }) {
 
 function PostMeta({ post }) {
   return (
-    <p className="text-[13px] text-gray-700 dark:text-gray-300">
-      {new Date(post.date).toLocaleDateString("en", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-      })}
-    </p>
+    <div className="flex flex-row items-center mt-1 mb-2">
+      <Link
+        className="flex flex-row items-center hover:underline"
+        href={`https://github.com/${post.author}`}
+        target="_blank"
+      >
+        <img
+          alt={`${post.author}`}
+          src={`https://github.com/${post.author}.png`}
+          className="relative mx-1 inline h-8 w-8 rounded-full"
+        />
+        <p className="text-[14px] ml-1 mr-1 font-medium">{post.author}</p>
+      </Link>
+      <p className="text-[14px] text-gray-700 dark:text-gray-300">
+        ·{" "}
+        {new Date(post.date).toLocaleDateString("en", {
+          day: "numeric",
+          month: "short",
+          year: "numeric",
+        })}
+      </p>
+    </div>
   );
 }
 
